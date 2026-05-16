1b1e288d68bbb9062e5b1b6fea9ebbceeb2a8728
gsucharitha2005 <gsucharitha2005@gmail.com>
2026-05-16 09:46:37 +0530

    update

--- converts.py
+++ converts.py
@@ -5,3 +5,32 @@ binary=bin(num)
 print(binary)
 
 print("hello")
+
+num=1233
+print(num)
+print("number type:", type(num))
+octal=oct(num)
+print(octal)
+
+print(".........")
+
+num=1233
+print(num)
+print("number type:", type(num))
+hex=hex(num)
+print(hex)
+
+print("..........")
+
+num=0x6ab
+print(num)
+print("number type:",type(num))
+binary=bin(num)
+print(binary)
+
+decimal=int(binary,2)
+print(decimal)
+octal=oct(decimal)
+print(octal)
+
+
